516ccd05b4cc75e3917a1f63765cdb4113b52cb9
FangDingli <1191230628@qq.com>
2021-07-06 19:08:23 +0800

    初始化，删了些不必要的东西

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
-	<div id="nav">
+  <!-- <div id="nav">
 		<router-link to="/">{{ $t("nav.home") }}</router-link> |
 		<router-link to="/about">{{ $t("nav.about") }}</router-link> |
 		<router-link to="/example">{{ $t("nav.example") }}</router-link>
-	</div>
-	<router-view />
-	<div hidden>我知道你肯定会修改这里的^.^</div>
+	</div> -->
+  <!-- <router-view /> -->
+  <!-- <div hidden>我知道你肯定会修改这里的^.^</div> -->
+  <NSpace vertical size="large">
+    <NLayout>
+      <NLayoutHeader>这是脑袋</NLayoutHeader>
+      <NLayoutContent>这是内容</NLayoutContent>
+    </NLayout>
+  </NSpace>
 </template>
 
 <script setup lang="ts">
-
+import { NSpace, NLayout, NLayoutHeader, NLayoutContent } from 'naive-ui'
 </script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-#nav { padding: 30px; }
-#nav a { font-weight: bold; color: #2c3e50; }
-#nav a.router-link-exact-active { color: #42b983; }
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
---
+++ src/components/SongList.vue
@@ -0,0 +1,31 @@
+<template>
+  <NDataTable :columns="columns"></NDataTable>
+</template>
+
+<script setup lang="ts">
+interface TableData {
+  songId: number
+  title: string
+  album: string
+  artist: string
+}
+
+import { h } from '@vue/runtime-core'
+import { NDataTable, NTag } from 'naive-ui'
+
+const columns = () => [
+  {
+    title: '歌名',
+    key: 'title',
+  },
+  {
+    title: '歌手',
+    key: 'artist',
+    render(row: TableData) {
+      return h(NTag, { type: 'success' }, { default: () => row.artist })
+    },
+  },
+]
+</script>
+
+<style></style>
